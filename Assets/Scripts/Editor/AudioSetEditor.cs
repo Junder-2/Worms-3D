8241e458f1fc6b07b4ccc00e8e4f1e944d36c452
School Computer <55179209+Junder-2@users.noreply.github.com>
2022-10-05 15:02:42 +0200

    Quick fix

--- Assets/Scripts/Editor/AudioSetEditor.cs
+++ Assets/Scripts/Editor/AudioSetEditor.cs
@@ -7,8 +7,8 @@ namespace Editor
     [CustomEditor(typeof(AudioSet))]
     public class AudioSetEditor : UnityEditor.Editor
     {
-        private readonly int audioEffectSize = Enum.GetValues(typeof(AudioSet.AudioID)).Length;
-        private readonly int songSize = Enum.GetValues(typeof(AudioSet.MusicID)).Length;
+        private readonly int _audioEffectSize = Enum.GetValues(typeof(AudioSet.AudioID)).Length;
+        private readonly int _songSize = Enum.GetValues(typeof(AudioSet.MusicID)).Length;
 
         private SerializedProperty _audioClips;
         private SerializedProperty _songClips;
@@ -16,13 +16,13 @@ namespace Editor
         private void OnEnable()
         {
             AudioSet audioSet = (AudioSet)target;
-            if (audioSet.audioClips.Length != audioEffectSize)
-                audioSet.audioClips = new AudioClip[audioEffectSize];
+            if (audioSet.audioClips.Length != _audioEffectSize)
+                audioSet.audioClips = new AudioClip[_audioEffectSize];
         
             _audioClips = serializedObject.FindProperty("audioClips");
 
-            if (audioSet.songClips.Length != songSize)
-                audioSet.songClips = new AudioClip[songSize];
+            if (audioSet.songClips.Length != _songSize)
+                audioSet.songClips = new AudioClip[_songSize];
         
             _songClips = serializedObject.FindProperty("songClips");
         }
@@ -31,7 +31,7 @@ namespace Editor
         {
             EditorGUI.BeginChangeCheck();
 
-            for (int i = 0; i < audioEffectSize; i++)
+            for (int i = 0; i < _audioEffectSize; i++)
             {
                 var clip = _audioClips.GetArrayElementAtIndex(i);
 
@@ -44,7 +44,7 @@ namespace Editor
 
             EditorGUILayout.Space(15);
 
-            for (int i = 0; i < songSize; i++)
+            for (int i = 0; i < _songSize; i++)
             {
                 var clip = _songClips.GetArrayElementAtIndex(i);
 
